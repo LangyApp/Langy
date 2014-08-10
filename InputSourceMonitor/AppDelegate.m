d746afb1229bc06de65d5707c488e5b17df7754c
Nicolás Santángelo <santangelonicolas@gmail.com>
2014-08-10 16:03:51 -0300

    Rename the app

--- InputSourceMonitor/AppDelegate.m
+++ InputSourceMonitor/AppDelegate.m
@@ -1,6 +1,6 @@
 //
 //  AppDelegate.m
-//  InputSourceChecker
+//  InputSourceMonitor
 //
 //  Created by Nicolas Santangelo on 8/6/14.
 //  Copyright (c) 2014 Nicolas Santangelo. All rights reserved.
--- InputSourceMonitorTests/InputSourceMonitorTests.m
+++ InputSourceMonitorTests/InputSourceMonitorTests.m
@@ -1,6 +1,6 @@
 //
-//  InputSourceCheckerTests.m
-//  InputSourceCheckerTests
+//  InputSourceMonitorTests.m
+//  InputSourceMonitorTests
 //
 //  Created by Nicolas Santangelo on 8/6/14.
 //  Copyright (c) 2014 Nicolas Santangelo. All rights reserved.
@@ -8,11 +8,11 @@
 
 #import <XCTest/XCTest.h>
 
-@interface InputSourceCheckerTests : XCTestCase
+@interface InputSourceMonitorTests : XCTestCase
 
 @end
 
-@implementation InputSourceCheckerTests
+@implementation InputSourceMonitorTests
 
 - (void)setUp
 {
